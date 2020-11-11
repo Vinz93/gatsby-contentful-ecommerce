7f571e96fb111008dad3da7c9d7b9fbf1a16e74e
Vincenzo Bianco <vincenzobianco1993@gmail.com>
2020-11-11 09:37:11 +0100

    remove console.log - prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,18 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-require('dotenv').config();
-
-console.log({
-  token: process.env.CONTENTFUL_ACCESS_TOKEN
-})
+require("dotenv").config()
 
 module.exports = {
   /* Your site config here */
-  plugins: [{
-   resolve: 'gatsby-source-contentful',
-   options: {
-     spaceId: 'u7a44gonzwk8',
-     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-     host: `preview.contentful.com`,
-   } 
-  }],
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: "u7a44gonzwk8",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
+      },
+    },
+  ],
 }
